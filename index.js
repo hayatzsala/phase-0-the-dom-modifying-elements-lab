00d30dbe7d28931267640d95ea2002ecf384a56f
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-08-29 02:38:28 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const specialMain = document.getElementById('main');
+specialMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Hayat is the champion";
+
+const body = document.querySelector('body');
+body.appendChild(newHeader);
